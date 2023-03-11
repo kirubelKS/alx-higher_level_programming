2e1e068ace153a47435f79dcf3f0c20f79d10a5b
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-11 12:10:46 +0300

    correction on Q8 and Q11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return "None"
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
     else:
-        max = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max:
-                max = my_list[i]
-        return max
+        del my_list[idx]
+    return my_list
