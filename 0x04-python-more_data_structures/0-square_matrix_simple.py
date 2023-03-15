20d2604ccdd54b14d5806bede3f9dc50e2898310
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-15 23:34:29 +0300

    project 0x04 more data more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -10,6 +10,6 @@ def square_matrix_simple(matrix=[]):
     new_lst = []
     if len(matrix) == 0:
         return new_lst
-
+    
     new_lst = [[i*i for i in j] for j in matrix]
     return new_lst
