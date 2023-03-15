dcbfe5aa1b8014b85805e82c2ee44e736c32a09c
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-16 00:19:12 +0300

    new try

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
+# 0-square_matrix_simple.py
+
 def square_matrix_simple(matrix=[]):
-    '''
-        A function that returns the square value of all elements in a
-        multi dimensional list been passed as arguments.
-        Returns a new matrix that is of same size as the size of the
-        matrix passed to it as arguments.
-        You are allowed to use regular loops, map, etc
-    '''
-    new_lst = []
-    if len(matrix) == 0:
-        return new_lst
-    
-    new_lst = [[i*i for i in j] for j in matrix]
-    return new_lst
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
