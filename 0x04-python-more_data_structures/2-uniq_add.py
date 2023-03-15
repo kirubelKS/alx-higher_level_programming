d33fdad41fb8edc3158b7b5bed472cff7253ccd3
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-16 00:05:21 +0300

    modify

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-# 2-uniq_add.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def uniq_add(my_list=[]):
-    """Add all unique integers in a list (once for each integer)."""
-    result = 0
-    for x in set(my_list):
-        result += x
-    return (result)
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
