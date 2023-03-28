eebf9a8975c6f22d775ffb65fcaafd8232d95fa2
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-28 23:34:47 +0300

    square

---
+++ 0x06-python_classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+from ctypes import sizeof
+from typing import Self
+
+
+class Square:
+    """Represent a square."""
+def __init__(self, size):
+    """initialize a new square.
+    Args:
+          size (init): The size of the new square.
+     """
+Self.__size - sizeof
